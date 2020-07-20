57862e8f087141e65f396be22e6157fd44c8b4b7
Lau <mlauri1997@gmail.com>
2020-07-20 18:02:50 -0300

    add

--- Producto/GreedyMe-Web/CodigoFuente/src/app.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CardInfo from "./CardInfo.js";
+import CardInfo from "./CardInfo";
 import { Router } from "@reach/router";
 
 const App = () => {
