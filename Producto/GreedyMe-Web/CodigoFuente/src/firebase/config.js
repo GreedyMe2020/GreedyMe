6a1bc46c171b819ba8447c7a53e0db9b23e88b50
YazimelNiclis <yazimel_niclis@hotmail.com>
2020-08-04 22:16:15 -0300

    Registro usuario
    Agregué registro usuario y un par de configuraciones del proyecto:
    + Lista de browsers compatibles.
    + Control de errores para Hooks.
    + Componente de front useDropdown para listas.

--- Producto/GreedyMe-Web/CodigoFuente/src/firebase/config.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/firebase/config.js
@@ -1,6 +1,15 @@
 import firebase from "firebase";
 
-const firebaseConfig = { null };
+const firebaseConfig = {
+  apiKey: "AIzaSyBMSuKle9DYdzJxk9t2GPxL98Ms296DgLU",
+  authDomain: "greedyme-d6c6c.firebaseapp.com",
+  databaseURL: "https://greedyme-d6c6c.firebaseio.com",
+  projectId: "greedyme-d6c6c",
+  storageBucket: "greedyme-d6c6c.appspot.com",
+  messagingSenderId: "984416387509",
+  appId: "1:984416387509:web:7855a6771b7a68999a99fc",
+  measurementId: "G-NFZ55KNHX9",
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
