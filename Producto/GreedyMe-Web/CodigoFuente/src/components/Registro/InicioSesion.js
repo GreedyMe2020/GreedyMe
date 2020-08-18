d32d599328b4b5d29834c0b0359590ecc05e6841
JuanCerutti <63431791+JuanCerutti@users.noreply.github.com>
2020-08-18 11:51:20 -0300

    card inicio sesion

--- Producto/GreedyMe-Web/CodigoFuente/src/components/Registro/InicioSesion.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/Registro/InicioSesion.js
@@ -7,6 +7,15 @@ import {
 import { connect } from "react-redux";
 import { signIn } from "../../redux/actions/authActions";
 import { Redirect, Link } from "@reach/router";
+import { Card } from "react-bootstrap";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import Grid from "@material-ui/core/Grid";
 
 function InicioSesion(props) {
   const [formData, setFormData] = React.useState({
@@ -23,37 +32,68 @@ function InicioSesion(props) {
     e.preventDefault();
     props.signIn(formData);
   };
-  console.log(props.auth);
-  //if (props.auth.uid) return <Redirect to={"/main/" + props.auth.uid} />;
+
+  const form = React.createRef();
+
+  if (props.auth.uid) return <Redirect to={"/main/" + props.auth.uid} />;
+
   return (
-    <div className="container">
-      <div className="login-container">
-        <h2>Inicia Sesion</h2>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Email"
-            className="email"
-            name="email"
-            onChange={handleChange}
-          ></input>
-          <input
-            type="password"
-            placeholder="Contraseña"
-            className="contraseña"
-            name="contraseña"
-            onChange={handleChange}
-          ></input>
-          <input
-            type="submit"
-            className="submit"
-            value="Iniciar Sesión"
-          ></input>
-        </form>
-        {props.authError ? <p>Le erraste en la contraseña o el mail</p> : null}
-      </div>
-      <Link to="/forgotpassword">¿Olvidaste tu contraseña?</Link>
-    </div>
+    <ValidatorForm ref={form} onSubmit={handleSubmit} id="validator-form">
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Iniciar Sesión</Card.Title>
+          <Grid item xs={12} md={6}>
+            <TextValidator
+              label="Email"
+              variant="standard"
+              onChange={handleChange}
+              name="email"
+              value={formData.email}
+              validators={["required", "isEmail"]}
+              errorMessages={[
+                "*Este campo es obligatorio",
+                "El email no es válido",
+              ]}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextValidator
+              label="Contraseña"
+              variant="standard"
+              onChange={handleChange}
+              name="contraseña"
+              value={formData.contraseña}
+              validators={["required"]}
+              errorMessages={["*Este campo es obligatorio"]}
+            />
+          </Grid>
+          <Typography color="textSecondary" gutterBottom>
+            <Link to="/forgotpassword">Olvidé mi contraseña</Link>
+          </Typography>
+          <FormControlLabel
+            control={
+              <Checkbox
+                icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
+                checkedIcon={<CheckBoxIcon fontSize="small" />}
+                name="checkedI"
+              />
+            }
+            label="Recuérdame"
+          />
+          <Button
+            variant="outlined"
+            className="mt-3 text-right"
+            color="secondary"
+            onClick={handleSubmit}
+          >
+            Iniciar sesión
+          </Button>
+          {props.authError ? (
+            <p className="text-danger">Los datos ingresados son incorrectos</p>
+          ) : null}
+        </Card.Body>
+      </Card>
+    </ValidatorForm>
   );
 }
 
