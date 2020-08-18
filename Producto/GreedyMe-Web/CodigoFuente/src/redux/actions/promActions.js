d06b36de2f415cf7335af8b6705f9d2fcb549d50
JuanCerutti <63431791+JuanCerutti@users.noreply.github.com>
2020-08-18 16:41:33 -0300

    actions de promociones

--- Producto/GreedyMe-Web/CodigoFuente/src/redux/actions/promActions.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/redux/actions/promActions.js
@@ -1,6 +1,39 @@
 export const crearPromocion = (promocion) => {
-  return (dispatch, getState) => {
-    //codigo asincrono
-    dispatch({ type: "CREAR_PROMOCION", promocion });
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("usuarioComercio")
+      .doc(promocion.id)
+      .collection("promociones")
+      .doc()
+      .set({
+        tipoPromocion: promocion.tipoPromocion,
+      })
+      .then(() => {
+        dispatch({ type: "CREAR_PROMOCION" });
+      })
+      .catch((error) => {
+        dispatch({ type: "ERROR_PROMOCION", error });
+      });
+  };
+};
+
+export const actulizarPromocion = (promocion) => {
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("usuarioComercio")
+      .doc(promocion.id)
+      .collection("promociones")
+      .doc()
+      .update({
+        tipoPromocion: promocion.tipoPromocion,
+      })
+      .then(() => {
+        dispatch({ type: "ACTUALIZAR_PROMOCION", promocion });
+      })
+      .catch((error) => {
+        dispatch({ type: "ERROR_ACTUALIZACION", error });
+      });
   };
 };
