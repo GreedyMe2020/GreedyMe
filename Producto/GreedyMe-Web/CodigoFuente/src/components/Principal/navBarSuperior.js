7657107a64c10f906c60636ac7780d2b18a934c0
Lau <mlauri1997@gmail.com>
2020-08-20 12:33:44 -0300

    navbar superior casi listo pero funcionando

--- Producto/GreedyMe-Web/CodigoFuente/src/components/Principal/navBarSuperior.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/Principal/navBarSuperior.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 
 export function NavBarSup() {
   return (
@@ -12,4 +12,269 @@ export function NavBarSup() {
       </nav>
     </div>
   );
+} */
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import Divider from "@material-ui/core/Divider";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ImageIcon from "@material-ui/icons/Image";
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "inline",
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+}));
+
+export function NavBarSup() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Nombre" />
+      </ListItem>
+      <Divider variant="middle" className="divider" />
+      <MenuItem onClick={handleMenuClose}>Mi perfil</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Suscripciones</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Ayuda y soporte técnico</MenuItem>
+      <Divider variant="middle" className="divider" />
+      <MenuItem onClick={handleMenuClose}>Cerrar sesión</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notificaciones</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Perfil</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar className="nav-container">
+          <div id="titulo">
+            <h1 className="gre">gre</h1>
+            <h1 className="edy">edy</h1>
+            <h1 className="me">me</h1>
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
 }
+
+/* 
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  sectionDesktop: {
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </div>
+  );
+} */
--- Producto/GreedyMe-Web/CodigoFuente/src/components/Promociones/cargaPromociones.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/Promociones/cargaPromociones.js
@@ -151,198 +151,190 @@ function CargaPromociones(props) {
         <div className="subtituloProm">
           <h4>Cargar la promoción o el descuento que aplica en su comercio</h4>
         </div>
-        <div className="contCargarProm">
-          <ValidatorForm
-            className={classes.root}
-            ref={form}
-            onSubmit={handleSubmit}
+
+        <ValidatorForm
+          className={classes.root}
+          ref={form}
+          onSubmit={handleSubmit}
+        >
+          <div className="contCargarProm">
+            <div className="contenedorCol1">
+              <SelectValidator
+                label="Tipo de promoción"
+                onChange={handleChange}
+                name="tipoPromo"
+                value={formData.tipoPromo}
+                validators={["required"]}
+                errorMessages={["*Este campo es obligatorio"]}
+              >
+                {tipoPromo.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </SelectValidator>
+
+              <SelectValidator
+                label="Proveedor de promoción"
+                onChange={handleChange}
+                name="proveedor"
+                value={formData.proveedor}
+                validators={["required"]}
+                errorMessages={["*Este campo es obligatorio"]}
+              >
+                {proveedor.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </SelectValidator>
+
+              <SelectValidator
+                label="Dia de vigencia"
+                onChange={handleChange}
+                name="diaVigencia"
+                value={formData.diaVigencia}
+                validators={["required"]}
+                errorMessages={["*Este campo es obligatorio"]}
+              >
+                {diaVigencia.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </SelectValidator>
+              <SelectValidator
+                label="Mes de vigencia"
+                onChange={handleChange}
+                name="mesVigencia"
+                value={formData.mesVigencia}
+                validators={["required"]}
+                errorMessages={["*Este campo es obligatorio"]}
+              >
+                {mesVigencia.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </SelectValidator>
+              <FormControlLabel
+                label="Efectivo"
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedEfectivo}
+                    onChange={handleChange}
+                    name="checkedEfectivo"
+                  />
+                }
+              />
+            </div>
+            <div className="contenedorCol2">
+              <p>¿Qué días aplica la promoción?</p>
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedTD}
+                    onChange={handleChange}
+                    name="checkedTD"
+                  />
+                }
+                label="Todos los días"
+              />
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedL}
+                    onChange={handleChange}
+                    name="checkedL"
+                  />
+                }
+                label="Lunes"
+              />
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedM}
+                    onChange={handleChange}
+                    name="checkedM"
+                  />
+                }
+                label="Martes"
+              />
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedMi}
+                    onChange={handleChange}
+                    name="checkedMi"
+                  />
+                }
+                label="Miércoles"
+              />
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedJ}
+                    onChange={handleChange}
+                    name="checkedJ"
+                  />
+                }
+                label="Jueves"
+              />
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedV}
+                    onChange={handleChange}
+                    name="checkedV"
+                  />
+                }
+                label="Viernes"
+              />
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedS}
+                    onChange={handleChange}
+                    name="checkedS"
+                  />
+                }
+                label="Sábado"
+              />
+              <FormControlLabel
+                control={
+                  <CelesteCheckbox
+                    checked={state.checkedD}
+                    onChange={handleChange}
+                    name="checkedD"
+                  />
+                }
+                label="Domingo"
+              />
+              <p>Agregar descripción</p>
+              <form className={classes.root} noValidate autoComplete="off">
+                <div>
+                  <TextField
+                    id="standard-textarea"
+                    label="Descripción (opcional)"
+                    placeholder="Descripción (opcional)"
+                    multiline
+                    rows={2}
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </ValidatorForm>
+
+        <div className="btnCargarProm">
+          <Button
+            color="primary"
+            variant="contained"
+            className={classes.margin}
+            type="submit"
           >
-            <Grid container>
-              <div className="contenedorCol1">
-                <Grid md={6}>
-                  <SelectValidator
-                    label="Tipo de promoción"
-                    onChange={handleChange}
-                    name="tipoPromo"
-                    value={formData.tipoPromo}
-                    validators={["required"]}
-                    errorMessages={["*Este campo es obligatorio"]}
-                  >
-                    {tipoPromo.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.value}
-                      </MenuItem>
-                    ))}
-                  </SelectValidator>
-                </Grid>
-                <Grid md={6}>
-                  <SelectValidator
-                    label="Proveedor de promoción"
-                    onChange={handleChange}
-                    name="proveedor"
-                    value={formData.proveedor}
-                    validators={["required"]}
-                    errorMessages={["*Este campo es obligatorio"]}
-                  >
-                    {proveedor.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.value}
-                      </MenuItem>
-                    ))}
-                  </SelectValidator>
-                </Grid>
-                <Grid md={6}>
-                  <SelectValidator
-                    label="Dia de vigencia"
-                    onChange={handleChange}
-                    name="diaVigencia"
-                    value={formData.diaVigencia}
-                    validators={["required"]}
-                    errorMessages={["*Este campo es obligatorio"]}
-                  >
-                    {diaVigencia.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.value}
-                      </MenuItem>
-                    ))}
-                  </SelectValidator>
-                </Grid>
-                <Grid md={6}>
-                  <SelectValidator
-                    label="Mes de vigencia"
-                    onChange={handleChange}
-                    name="mesVigencia"
-                    value={formData.mesVigencia}
-                    validators={["required"]}
-                    errorMessages={["*Este campo es obligatorio"]}
-                  >
-                    {mesVigencia.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.value}
-                      </MenuItem>
-                    ))}
-                  </SelectValidator>
-                </Grid>
-                <FormControlLabel
-                  label="Efectivo"
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedEfectivo}
-                      onChange={handleChange}
-                      name="checkedEfectivo"
-                    />
-                  }
-                />
-              </div>
-              <div className="contenedorCol2">
-                <p>¿Qué días aplica la promoción?</p>
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedTD}
-                      onChange={handleChange}
-                      name="checkedTD"
-                    />
-                  }
-                  label="Todos los días"
-                />
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedL}
-                      onChange={handleChange}
-                      name="checkedL"
-                    />
-                  }
-                  label="Lunes"
-                />
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedM}
-                      onChange={handleChange}
-                      name="checkedM"
-                    />
-                  }
-                  label="Martes"
-                />
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedMi}
-                      onChange={handleChange}
-                      name="checkedMi"
-                    />
-                  }
-                  label="Miércoles"
-                />
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedJ}
-                      onChange={handleChange}
-                      name="checkedJ"
-                    />
-                  }
-                  label="Jueves"
-                />
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedV}
-                      onChange={handleChange}
-                      name="checkedV"
-                    />
-                  }
-                  label="Viernes"
-                />
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedS}
-                      onChange={handleChange}
-                      name="checkedS"
-                    />
-                  }
-                  label="Sábado"
-                />
-                <FormControlLabel
-                  control={
-                    <CelesteCheckbox
-                      checked={state.checkedD}
-                      onChange={handleChange}
-                      name="checkedD"
-                    />
-                  }
-                  label="Domingo"
-                />
-                <p>Agregar descripción</p>
-                <form className={classes.root} noValidate autoComplete="off">
-                  <div>
-                    <TextField
-                      id="standard-textarea"
-                      label="Descripción (opcional)"
-                      placeholder="Descripción (opcional)"
-                      multiline
-                      rows={2}
-                    />
-                  </div>
-                </form>
-              </div>
-              <div className="btnCargarProm">
-                <Grid item>
-                  <Button
-                    color="primary"
-                    variant="contained"
-                    className={classes.margin}
-                    type="submit"
-                  >
-                    Enviar
-                  </Button>
-                </Grid>
-              </div>
-            </Grid>
-          </ValidatorForm>
+            Enviar
+          </Button>
         </div>
       </div>
     </div>
