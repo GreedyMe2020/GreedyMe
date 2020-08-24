c9a1ae3abf275dc3e6898ea4fec29a641bfff80b
JuampiZablosky <44734692+JuampiZablosky@users.noreply.github.com>
2020-08-23 23:27:18 -0300

    Actualizo estilo de cargar promociones

--- Producto/GreedyMe-Web/CodigoFuente/src/components/Principal/navBarSuperior.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/Principal/navBarSuperior.js
@@ -1,11 +1,6 @@
 import * as React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Divider from "@material-ui/core/Divider";
-import List from "@material-ui/core/List";
-import Drawer from "@material-ui/core/Drawer";
-import ListItemCustom from "../ListItemCustom";
 import { Notificaciones } from "../Notificaciones";
 import Perfil from "../Perfil";
 import { Link } from "@reach/router";
