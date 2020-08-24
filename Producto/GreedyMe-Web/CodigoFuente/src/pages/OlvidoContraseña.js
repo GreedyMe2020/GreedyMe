1bdbf926bd8167819c515c58d52415d0c2708939
YazimelNiclis <yazimel_niclis@hotmail.com>
2020-08-24 11:53:06 -0300

    asd

--- Producto/GreedyMe-Web/CodigoFuente/src/pages/OlvidoContraseña.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReestablecerContraseña from "../components/Registro/ReestablecerContraseña";
-
-export function OlvidoContraseña() {
-  return <ReestablecerContraseña />;
-}
--- Producto/GreedyMe-Web/CodigoFuente/src/pages/Registro.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import InicioSesion from "../components/Registro/InicioSesion";
-
-export function Registro() {
-  return <InicioSesion />;
-}
