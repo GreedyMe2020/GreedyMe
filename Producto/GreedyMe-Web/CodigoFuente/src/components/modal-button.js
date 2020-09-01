9ab6b49d3abb929d9764f4503bc414cbd9cf775b
Lau <mlauri1997@gmail.com>
2020-09-01 20:43:47 -0300

    limpie codigo

--- Producto/GreedyMe-Web/CodigoFuente/src/components/modal-button.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/modal-button.js
@@ -5,6 +5,7 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
+import { useTheme } from "@material-ui/core/styles";
 import ModalPromociones from "../components/Promociones/modalPromociones";
 
 export const ModalButton = ({ children }) => {
