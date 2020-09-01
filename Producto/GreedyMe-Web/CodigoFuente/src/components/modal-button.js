2779253f1559ed7bd41bcb3c0870ddb999b60c68
Lau <mlauri1997@gmail.com>
2020-09-01 20:41:38 -0300

    limpie codigo

--- Producto/GreedyMe-Web/CodigoFuente/src/components/modal-button.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/modal-button.js
@@ -1,16 +1,10 @@
 import React, { useState } from "react";
 import { MyModalWithGrid } from "./Modal";
-
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
-import AddIcon from "@material-ui/icons/Add";
-import Fab from "@material-ui/core/Fab";
 import ModalPromociones from "../components/Promociones/modalPromociones";
 
 export const ModalButton = ({ children }) => {
