4cde020d0970a3af50ed971c778f0c94c7e8e528
Lau <mlauri1997@gmail.com>
2020-09-14 00:14:43 -0300

    sumando lineas

--- Producto/GreedyMe-Web/CodigoFuente/src/components/Notificaciones/programarNotif.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/Notificaciones/programarNotif.js
@@ -5,6 +5,14 @@ import CardContent from "@material-ui/core/CardContent";
 import Switch from "@material-ui/core/Switch";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import Paper from "@material-ui/core/Paper";
+import {
+  ValidatorForm,
+  SelectValidator,
+} from "react-material-ui-form-validator";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
@@ -27,10 +35,37 @@ const useStyles = makeStyles((theme) => ({
   demo: {
     backgroundColor: theme.palette.background.paper,
   },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
 }));
 
 function Notificaciones() {
   const classes = useStyles();
+  /* const [formData, setFormData] = React.useState({
+    id: props.auth.uid,
+    usuario: "",
+    sucursal: "",
+    tipoCliente: "",
+    promocion: "",
+    programarEnvio: "",
+    ubicacion: "",
+  }); */
+
+  const promociones = [{ name: "Promociones" }, { name: "Descuentos" }];
+
+  const options = promociones.map((option) => {
+    const firstLetter = option.name[0].toUpperCase();
+    return {
+      firstLetter: /[0-9]/.test(firstLetter) ? "0-9" : firstLetter,
+      ...option,
+    };
+  });
+
   //Estados
 
   //Estado checked del switch de geolocalizacion
@@ -43,6 +78,21 @@ function Notificaciones() {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
+  const handleChangeCliente = (event) => {
+    formData[event.target.name] = event.target.value;
+    setFormData({ ...formData });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // props.editarDatos(formData);
+    // setSubmitted({ submitted: true }, () => {
+    //   setTimeout(() => setSubmitted({ submitted: false }), 5000);
+    // });
+  };
+
+  const form = React.createRef();
+
   return (
     <div>
       <div className="prom-title-container">
@@ -51,38 +101,111 @@ function Notificaciones() {
       <div className="subtitulo-notif">
         <h6>Gestioná las notificaciones push que enviás a los usuarios</h6>
       </div>
-      <div className="contenedor-todo-notif">
-        <Card className="card-notif">
-          <CardContent className="card-content-notif">
-            <div>
-              <FormControlLabel
-                value="activGeoloc"
-                control={
-                  <Switch
-                    color="primary"
-                    checked={state.activo}
-                    onChange={handleChange}
-                    name="activo"
+      <ValidatorForm ref={form} onSubmit={handleSubmit}>
+        <div className="contenedor-todo-notif">
+          <Card className="card-notif">
+            <CardContent className="card-content-notif">
+              <div>
+                <FormControlLabel
+                  value="activGeoloc"
+                  control={
+                    <Switch
+                      color="primary"
+                      checked={state.activo}
+                      onChange={handleChange}
+                      name="activo"
+                    />
+                  }
+                  label="Notificaciones por ubicación cercana"
+                  labelPlacement="start"
+                />
+              </div>
+              <div className="input-nom-suc ">
+                <TextField
+                  disabled
+                  fullWidth
+                  className="text-usuario"
+                  id="outlined-disabled"
+                  label="Nombre del comercio"
+                  value="Adidongas"
+                  //value={props.profile.nombreComercio}
+                  variant="outlined"
+                  name="usuario"
+                />
+                <TextField
+                  disabled
+                  fullWidth
+                  className="text-sucursal"
+                  id="outlined-disabled"
+                  label="Sucursal"
+                  value="Pationgo Olmos"
+                  //value={props.profile.sucursal}
+                  variant="outlined"
+                  name="sucursal"
+                />
+              </div>
+              <div className="input-tipo-clientes">
+                <h6 className="texto-tipo-cliente">
+                  La notificación está dirigida a:
+                </h6>
+                <SelectValidator
+                  className="select-tipo-cliente"
+                  fullWidth
+                  label="Tipo de cliente"
+                  onChange={handleChangeCliente}
+                  name="tipoCliente"
+                  //value={formData.tipoCliente}
+                  variant="outlined"
+                  validators={["required"]}
+                  errorMessages={["*Este campo es obligatorio"]}
+                >
+                  {/* ACA DEBERIA IR "TODOS LOS USUARIOS o USUARIOS DE LOCALES FAVORITOS
+                {proveedor.map((option) => (
+                  <MenuItem key={option.tipo} value={option.tipo}>
+                    {option.tipo}
+                  </MenuItem>
+                ))} */}
+                </SelectValidator>
+              </div>
+              <div className="input-buscador-beneficio">
+                <h6 className="texto-beneficio">
+                  Beneficio que va a notificar:
+                </h6>
+                <Autocomplete
+                  id="buscador-beneficio"
+                  options={options.sort(
+                    (a, b) => -b.firstLetter.localeCompare(a.firstLetter)
+                  )}
+                  groupBy={(option) => option.firstLetter}
+                  getOptionLabel={(option) => option.name}
+                  style={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Beneficio"
+                      variant="outlined"
+                    />
+                  )}
+                />
+                {/* <Paper id="buscador-beneficio">
+                  <InputBase
+                    className={classes.input}
+                    placeholder="Buscar beneficio..."
+                    inputProps={{ "aria-label": "buscar beneficio" }}
                   />
-                }
-                label="Notificaciones por ubicación cercana"
-                labelPlacement="start"
-              />
-            </div>
-            <div className="inputPerfil">
-              <TextField
-                disabled
-                id="outlined-disabled"
-                label="Nombre del comercio"
-                value="Adidongas"
-                //value={props.profile.nombreComercio}
-                variant="outlined"
-                name="usuario"
-              />
-            </div>
-          </CardContent>
-        </Card>
-      </div>
+                  <IconButton
+                    type="submit"
+                    className={classes.iconButton}
+                    aria-label="search"
+                  >
+                    <SearchIcon />
+                  </IconButton>
+                </Paper> */}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </ValidatorForm>
     </div>
   );
 }
