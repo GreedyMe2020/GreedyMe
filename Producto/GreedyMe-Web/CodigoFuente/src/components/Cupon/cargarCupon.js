4bd35e2dd9120d4cc67c9f628f982ea318920a73
Lau <mlauri1997@gmail.com>
2020-09-17 17:00:16 -0300

    cupon

--- Producto/GreedyMe-Web/CodigoFuente/src/components/Cupon/cargarCupon.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/Cupon/cargarCupon.js
@@ -1,14 +1,59 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 function Cupon() {
+  const beneficios = [
+    { name: "3x2 Club Personal, válida desde el" },
+    { name: "20% Nuevo club, válida desde el" },
+    { name: "5*2 Club La Voz, válida desde el" },
+    { name: "4x2 Talleres, válida desde el" },
+    { name: "APEPE La Voz, válida desde el" },
+    { name: "PEPE La Voz, válida desde el" },
+    { name: "1x2 Club Personal, válida desde el" },
+    { name: "4x2 OLA, válida desde el" },
+    { name: "5*2 PEPE La Voz, válida desde el" },
+  ];
+
+  const options = beneficios.map((option) => {
+    const firstLetter = option.name[0].toUpperCase();
+    return {
+      firstLetter: /[0-9]/.test(firstLetter) ? firstLetter : firstLetter,
+      ...option,
+    };
+  });
   return (
     <div>
       <div className="prom-title-container">
         <h1>Cupones</h1>
       </div>
-      <div className="subtitulo-notif">
-        <h6>Genera un nuevo cupon para tu usuario</h6>
-      </div>
+      <Card className="card-cupon">
+        <CardContent className="card-content-cupon">
+          <div className="input-buscador-beneficio">
+            <h6 className="texto-beneficio">Beneficio que va a notificar:</h6>
+            <Autocomplete
+              className="buscador-ben"
+              fullWidth
+              validators={["required"]}
+              errorMessages={["*Este campo es obligatorio"]}
+              options={options.sort(
+                (a, b) => -b.firstLetter.localeCompare(a.firstLetter)
+              )}
+              groupBy={(option) => option.firstLetter}
+              getOptionLabel={(option) => option.name}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Buscar beneficio"
+                  variant="outlined"
+                />
+              )}
+            />
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
