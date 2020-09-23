7032ef89fa0598e285dde5be2aaf95c2f81e7f7a
YazimelNiclis <yazimel_niclis@hotmail.com>
2020-09-23 19:25:12 -0300

    analytics

--- Producto/GreedyMe-Web/CodigoFuente/src/firebase/config.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/firebase/config.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
+import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMSuKle9DYdzJxk9t2GPxL98Ms296DgLU",
