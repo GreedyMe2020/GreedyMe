910b0594f1d55892275152e9bd0c82b0211a390f
JuampiZablosky <44734692+JuampiZablosky@users.noreply.github.com>
2021-01-18 22:23:19 -0300

    Agrego componente de productos canjeados y de puntos de entrega

--- Producto/GreedyMe-Web/CodigoFuente/src/components/Administrador/GreedyShop/PuntosEntrega.js
+++ Producto/GreedyMe-Web/CodigoFuente/src/components/Administrador/GreedyShop/PuntosEntrega.js
@@ -1,9 +1,229 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Tooltip from "@material-ui/core/Tooltip";
+import DialogComponent from "../../Dialog";
+import Dialog from "@material-ui/core/Dialog";
+import { Grid, Avatar, IconButton } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { connect } from "react-redux";
+import Typography from "@material-ui/core/Typography";
+import ModalAdministrador from "../modal-admin";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import CreateIcon from "@material-ui/icons/Create";
+import CloseIcon from "@material-ui/icons/Close";
+/* import ModalActualizarComercio from "../Comercios/modal-actualizar-comercio"; */
+import {
+  signUp,
+  eliminarUsuarioComercio,
+  modificarUsuarioComercio,
+} from "../../../redux/actions/adminActions";
+import FormPuntoEntrega from "./FormPuntosEntrega";
+import Snackbar from "@material-ui/core/Snackbar";
+import firebase from "../../../firebase/config";
+import _ from "lodash";
+//esta es la funcion que trae los datos, tipo crea un array trae todos las promociones
+//y la va acumulando en el array
 
-export default function PuntosEntrega(){
-    return(
-        <div>
-            <h5>Puntos entrega</h5>
-        </div>
-    );
+const useStyles = makeStyles((theme) => ({
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  cruz: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: "8px",
+    color: theme.palette.grey[500],
+  },
+  inline: {
+    display: "block",
+  },
+}));
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export default function PuntosEntrega(props) {
+  const classes = useStyles();
+
+  //Estado del dialog (abierto/cerrado) y propiedades del dialog
+  const [open, setOpen] = React.useState(false);
+  const [fullWidth, setFullWidth] = React.useState(true);
+  const [maxWidth, setMaxWidth] = React.useState("xs");
+
+  //estado de alerta
+  const [openAlert, setOpenAlert] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleMouseDownPromo = (event) => {
+    event.preventDefault();
+  };
+
+  const handleCloseAlert = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setEliminada(false);
+  };
+
+  //estados solo para el buscador
+
+  const [listaProductos, setListaProductos] = React.useState(null);
+  const [texto, setTexto] = React.useState(false);
+  const [text, setText] = React.useState("");
+
+  //Estados para setear producto a eliminar, y eliminarlo
+  const [eliminar, setEliminar] = React.useState(null);
+  const [currentId, setCurrentId] = React.useState(null);
+
+  //Snackbar cuando se elimina un producto
+  const [eliminada, setEliminada] = React.useState(false);
+
+  //Eliminar un producto de la BD y renderizar la eliminacion (cambiarlo para los productos)
+  /* React.useEffect(() => {
+    if (currentId) {
+      props.eliminarUsuarioComercio({
+        id: currentId,
+      });
+    }
+  }, [currentId]); */
+
+  //funcion para buscar
+  /* const filter = (text) => {
+    if (props.usuarios) {
+      let textoBuscar = text.target.value;
+      const datos = props.usuarios;
+      const newDatos = datos.filter(function (item) {
+        const itemNombreComercio = item.nombreComercio.toUpperCase();
+        const itemRubro = item.rubro.toUpperCase();
+        const campo = itemNombreComercio + " " + itemRubro;
+        const textData = textoBuscar.toUpperCase();
+        return campo.indexOf(textData) > -1;
+      });
+      setListaUsuarios(newDatos);
+      setText(text);
+      if (text.target.value != "") {
+        setTexto(true);
+      } else {
+        setTexto(false);
+      }
+    }
+  }; */
+
+  /* const crearComercio = (formData) => {
+    props.signUp(formData);
+  };
+
+  const actualizarComercio = (formData) => {
+    props.modificarUsuarioComercio(formData);
+  }; */
+
+  return (
+    <div>
+      <ModalAdministrador
+        title="Puntos de retiro"
+        titleModal="Nuevo punto de retiro"
+        button="Nuevo punto de retiro"
+        openContent={<FormPuntoEntrega />}
+        placeholder="Buscar punto de retiro…"
+        width="xs"
+        /* onChange={(text) => filter(text)} */
+      />
+
+      <div className="contenedorTodo">
+        <Card className="cardPromo">
+          <CardContent className="cardContentePromo">
+            <Grid item xs={12} md={12}>
+              <div className={classes.demo}>
+                <List>
+                    <ListItem key={1}>
+                    <ListItemAvatar>
+                        <Avatar
+                        variant="square"
+                        src={require("../../../../Multimedia/Sistema-svg/location.svg")}
+                        ></Avatar>
+                    </ListItemAvatar>
+                    <div className="elementoListaProm">
+                        <ListItemText
+                        //asi podes ir accediendo a todos los datos asi los acomodas como quieras
+                        primary={
+                            <React.Fragment>
+                            <Typography className={classes.inline}>
+                                Los Pumas 520, Villa Allende.
+                            </Typography>
+                            </React.Fragment>
+                        }
+                        secondary={"Córdoba, Argentina."}
+                        />
+                    </div>
+                    <ListItemSecondaryAction>
+                        <Tooltip title="Eliminar" arrow>
+                        <IconButton
+                            onClick={() => {
+                            /* setEliminar(user.id); */
+                            setOpen(true);
+                            /* console.log(user.id); */
+                            }}
+                            edge="end"
+                            aria-label="Eliminar"
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                        </Tooltip>
+                        <DialogComponent
+                        open={open}
+                        setOpen={setOpen}
+                        handleClose={handleClose}
+                        eliminar={eliminar}
+                        setEliminar={setEliminar}
+                        setEliminada={setEliminada}
+                        setCurrentId={setCurrentId}
+                        title={"¿Estás seguro de eliminar el punto de retiro?"}
+                        text={
+                            "Una vez que aceptes eliminar el punto de retiro, el mismo no podrá ser recuperado."
+                        }
+                        btnText={"Eliminar"}
+                        />
+                    </ListItemSecondaryAction>
+                    </ListItem>
+                </List>
+                {eliminada ? (
+                  <Snackbar
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                    open={eliminada}
+                    autoHideDuration={7000}
+                    onClose={handleCloseAlert}
+                  >
+                    <Alert onClose={handleCloseAlert} severity="error">
+                      El punto de retiro se ha eliminado
+                    </Alert>
+                  </Snackbar>
+                ) : (
+                  ""
+                )}
+              </div>
+            </Grid>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
